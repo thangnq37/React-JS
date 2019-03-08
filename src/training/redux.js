0a4f89981498c6ef1d9d21a79a4be5ffb617722a
thangnq37 <nguyenquocthang3794@gmail.com>
2019-03-08 07:38:57 +0700

    add_redux_2

--- src/training/redux.js
+++ src/training/redux.js
@@ -1,12 +1,24 @@
 import {createStore} from 'redux';
 
 var initialState = {
-	status: false
+	status: false,
+	sort: {
+		name: "test",
+		status: true
+	}
 }
 
 var myReducer = (state = initialState, action) => {
 	if(action.type === "APPLY_STATE"){
 		state.status = !state.status;
+	}
+
+	if(action.type === "ACTION_STATE"){
+		
+		state.sort = {
+			name: action.sort.name,
+			status: action.sort.status
+		}
 	}
 	return state;
 }
@@ -16,6 +28,20 @@ console.log("Test1",store.getState());
 
 var action = {type: "APPLY_STATE"};
 
+
+
 store.dispatch(action);
 
 console.log("Test2",store.getState());
+
+var action2 = {
+	type: "ACTION_STATE",
+	sort: {
+		name: "khoai",
+		status: false
+	}
+}
+
+store.dispatch(action2);
+
+console.log("Test3",store.getState());
